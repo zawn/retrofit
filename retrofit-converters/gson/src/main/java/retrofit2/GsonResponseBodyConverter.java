7cda081813c5689bdf829094a98c27ff4ed32c21
Jake Wharton <jw@squareup.com>
2016-01-18 01:09:43 -0500

    WIP

--- retrofit-converters/gson/src/main/java/retrofit2/GsonResponseBodyConverter.java
+++ retrofit-converters/gson/src/main/java/retrofit2/GsonResponseBodyConverter.java
@@ -15,20 +15,25 @@
  */
 package retrofit2;
 
+import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
 import java.io.IOException;
 import okhttp3.ResponseBody;
 
 final class GsonResponseBodyConverter<T> implements Converter<ResponseBody, T> {
+  private final Gson gson;
   private final TypeAdapter<T> adapter;
 
-  GsonResponseBodyConverter(TypeAdapter<T> adapter) {
+  GsonResponseBodyConverter(Gson gson, TypeAdapter<T> adapter) {
+    this.gson = gson;
     this.adapter = adapter;
   }
 
   @Override public T convert(ResponseBody value) throws IOException {
     try {
-      return adapter.fromJson(value.charStream());
+      JsonReader reader = gson.newJsonReader(value.charStream());
+      return adapter.fromJson(reader);
     } finally {
       value.close();
     }
